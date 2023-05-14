902fda40495a1c0afd3530ec2486d7f5534e5a6d
Lukáš Mikuláštík <mikulastik@slajit.cz>
2023-05-14 03:22:00 +0200

    fix basic header layout

--- src/components/Header/InstantBookable/InstantBookable.styles.ts
+++ src/components/Header/InstantBookable/InstantBookable.styles.ts
@@ -1,5 +1,5 @@
 import styled from "styled-components";
 
 export const StyledInstantBookable = styled.div`
-  padding: 1rem;
+  padding: 1rem 0 1rem 1rem;
 `
--- src/components/Header/PriceSlider/PriceSlider.styles.tsx
+++ src/components/Header/PriceSlider/PriceSlider.styles.tsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
 export const StyledPriceList = styled.div`
-  padding: 1rem;
+  padding: 1rem 1rem 1rem 0rem;
   width: 25%;
 `
